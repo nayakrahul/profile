180d0f70ddbeb7989b084e8bee1f28c1edf65cb6
Rahul Nayak <rahulnk521@gmail.com>
2018-09-19 12:22:18 +0530

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,8 +16,8 @@ Enjoy responsibly!
 
 $(document).ready(function () {
 
-	setTimeout(function () {
-		$('body').addClass('loaded');
-		$('h1').css('color', '#222222');
-	}, 3000);
+  setTimeout(function () {
+    $('body').addClass('loaded');
+    $('h1').css('color', '#222222');
+  }, 3000);
 });

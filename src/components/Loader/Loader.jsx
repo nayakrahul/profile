8f0e6fd87fdc5cbcbf6c4de203e2d64c59dadc07
Rahul Nayak <rahulnk521@gmail.com>
2018-10-24 15:04:44 +0530

    some changes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,9 +4,19 @@ import './Loader.scss';
 
 
 class Loader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const height = document.getElementById('app').clientHeight;
+    this.setState({loaderStyle: {minHeight: height}});
+  }
+
   render() {
     return (
-      <div id="loader-wrapper">
+      <div id="loader-wrapper" style={this.state.loaderStyle}>
         <div className="box5"><Bars/></div>
         <div className="loader-section section-left"></div>
         <div className="loader-section section-right"></div>
